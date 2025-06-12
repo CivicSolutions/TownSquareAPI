e9a638aa23d7ec90a3c7dfad71ef0fde95737ad1
Kai Parkinson Schürer <kaischurer7@gmail.com>
2025-06-12 09:26:52 +0200

    Added: DTOs der ProfilePicture

---
+++ TownSquareAPI/DTOs/ProfilePicture/ProfilePictureRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TownSquareAPI.DTOs.ProfilePicture;
+
+public class ProfilePictureRequestDTO
+{
+    public byte[] Picture { get; set; }
+    public int UserId { get; set; }
+    public bool IsDeafult { get; set; }
+}
---
+++ TownSquareAPI/DTOs/ProfilePicture/ProfilePictureResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TownSquareAPI.DTOs.ProfilePicture;
+
+public class ProfilePictureResponseDTO
+{
+    public int Id { get; set; }
+    public byte[] Picture { get; set; }
+    public int UserId { get; set; }
+    public bool IsDeafult { get; set; }
+}
