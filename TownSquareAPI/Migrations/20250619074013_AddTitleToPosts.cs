5abfd639f6298a88074bfa15353f9991359a1747
Dominik Meister <dominik.s.meister@gmail.com>
2025-06-19 09:53:31 +0200

    fix: add Title attribute to Posts

---
+++ TownSquareAPI/Migrations/20250619074013_AddTitleToPosts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TownSquareAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTitleToPosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Post",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Post");
+        }
+    }
+}
